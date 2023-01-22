00ae3b17853ab49b9369b013f49593715228bcd2
Naiara <naiarataianelimaamorim@gmail.com>
2023-01-22 19:52:55 -0300

    adicionando funções add e save

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 const form = document.querySelector('#forms-ts')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-01', '01-02', '01-06', '01-07'],
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    //const today = '13/01'
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert('Dia já incluso')
+        return
+    }
+
+    alert('Adicionado com Sucesso')
+    nlwSetup.addDay(today)
 }
 
+function save(){
+   localStorage.setItem('NLWSite@habits', JSON.stringify(nlwSetup.data)) // passando para String
+
+
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSite@habits')) || {} // transformando em objeto novamente
 nlwSetup.setData(data)
 nlwSetup.load()
+
+
+/*
+const data = {
+    run: ['01-01', '01-02', '01-06', '01-07', '01-08', '01-09', '01-10','01-11','01-12','01-13','01-14',],
+}*/
+
+const date = new Date()
+alert(date)
+
+
